2dc6ccb69aed19ada0371a9f1d24280ccaf79070
deibetheprogrammer <45319861+deibetheprogrammer@users.noreply.github.com>
2020-11-14 19:13:08 -0600

    Clase campo creada

---
+++ proyecto/src/proyecto/Campo.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author daba5
+ */
+public class Campo {
+    
+    //ATRIBUTOS
+    private String nombre;
+    private String tipo;
+    private int bytes;
+    private boolean Lprimaria;
+    
+    //CONSTRUCTOR
+    public Campo() {
+        
+    }
+
+    public Campo(String nombre, String tipo, int bytes, boolean Lprimaria) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.bytes = bytes;
+        this.Lprimaria = Lprimaria;
+    }
+    
+    //GETTERS Y SETTERS
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getBytes() {
+        return bytes;
+    }
+
+    public void setBytes(int bytes) {
+        this.bytes = bytes;
+    }
+
+    public boolean isLprimaria() {
+        return Lprimaria;
+    }
+
+    public void setLprimaria(boolean Lprimaria) {
+        this.Lprimaria = Lprimaria;
+    }
+    
+    
+}
