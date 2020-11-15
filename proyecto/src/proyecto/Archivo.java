8230da53d0b27934b84db2617aeee09e69a30f5a
Jose Zuniga <zunigajose21@gmail.com>
2020-11-14 21:11:46 -0600

    Clase Archivos dos cosas

--- proyecto/src/proyecto/Archivo.java
+++ proyecto/src/proyecto/Archivo.java
@@ -1,8 +1,9 @@
 package proyecto;
 
+//import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Archivo {
+public class Archivo /*implements Serializable*/ {
 
     ArrayList<Campo> ListaCampos = new ArrayList();
 
@@ -18,5 +19,11 @@ public class Archivo {
         this.ListaCampos = ListaCampos;
     }
 
-    
+    public Campo getListaCampo(int pos) {
+        return ListaCampos.get(pos);
+    }
+
+    public void setListaCampo(Campo Campo) {
+        this.ListaCampos.add(Campo);
+    }
 }
