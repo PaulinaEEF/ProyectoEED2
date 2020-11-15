2a06e489197e75fc49bd8dbb561316a7fd431b23
Juda <judaponce14@unitec.edu>
2020-11-14 21:04:20 -0600

    clase archivo

---
+++ proyecto/src/proyecto/Archivo.java
@@ -0,0 +1,22 @@
+package proyecto;
+
+import java.util.ArrayList;
+
+public class Archivo {
+
+    ArrayList<Campo> ListaCampos = new ArrayList();
+
+    public Archivo() {
+
+    }
+
+    public ArrayList<Campo> getListaCampos() {
+        return ListaCampos;
+    }
+
+    public void setListaCampos(ArrayList<Campo> ListaCampos) {
+        this.ListaCampos = ListaCampos;
+    }
+
+    
+}
