a88ffd83d0ffaea71746b5f843363d926ba85ea2
David Bendeck <dabap504@gmail.com>
2020-12-18 10:49:41 -0600

    EliminarDebug

--- proyecto/src/proyecto/VentanaPrincipal.java
+++ proyecto/src/proyecto/VentanaPrincipal.java
@@ -2616,6 +2616,7 @@ public class VentanaPrincipal extends javax.swing.JFrame {
                         arbolitos.get(getPosKey()).Remove(llave);
                         
                         rewrite(new String(data2), Math.toIntExact(rrn));
+                        archivoFalso.getAvailList().add(0, Math.toIntExact(rrn));
                     } catch (IOException ex) {
                         Logger.getLogger(VentanaPrincipal.class.getName()).log(Level.SEVERE, null, ex);
                     }
@@ -2625,7 +2626,6 @@ public class VentanaPrincipal extends javax.swing.JFrame {
             Eliminar_textfield.setEditable(true);
             cb_camposLlave.setEnabled(true);
             Eliminar_Buscar.setEnabled(true);
-            archivoFalso.getAvailList().add(0, rrnEli);
             Eliminar_Borrar.setEnabled(false);
         }
 
